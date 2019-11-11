66a1f12f816818e7ccb8d1bae1da8308770abe8f
json <json@healingpaper.com>
2019-11-11 23:15:43 +0900

    Chapter 2 - tests with @Before

--- test/maytree/ProfileTest.java
+++ test/maytree/ProfileTest.java
@@ -2,37 +2,49 @@ package maytree;
 
 import static org.junit.Assert.*;
 
+import java.util.logging.Logger;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class ProfileTest {
+  static private Logger logger = Logger.getLogger(ProfileTest.class.getName());
+
+  private Profile profile;
+  private BooleanQuestion question;
+  private Criteria criteria;
+
+  @Before
+  public void create() {
+    this.profile = new Profile("Bull Hockey, Inc.");
+    this.question = new BooleanQuestion(1, "Got bounses?");
+    this.criteria = new Criteria();
+
+    logger.info("called twice");
+  }
+
   @Test
   public void matchAnswersFalseWhenMustMatchCriteriaNotMet() {
-    Profile profile = new Profile("Bull Hockey, Inc.");
-    Question question = new BooleanQuestion(1, "Got bounses?");
-    Answer profileAnswer = new Answer(question, Bool.FALSE);
+    Answer profileAnswer = new Answer(this.question, Bool.FALSE);
     profile.add(profileAnswer);
-    Criteria criteria = new Criteria();
-    Answer criteriaAnswer = new Answer(question, Bool.TRUE);
+    Answer criteriaAnswer = new Answer(this.question, Bool.TRUE);
     Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
-    criteria.add(criterion);
+    this.criteria.add(criterion);
 
-    boolean matches = profile.matches(criteria);
+    boolean matches = profile.matches(this.criteria);
 
     assertFalse(matches);
   }
 
   @Test
   public void matchAnswersTrueForAnyDontCareCriteria() {
-    Profile profile = new Profile("Bull Hockey, Inc.");
-    Question question = new BooleanQuestion(1, "Got milk?");
-    Answer profileAnswer = new Answer(question, Bool.FALSE);
+    Answer profileAnswer = new Answer(this.question, Bool.FALSE);
     profile.add(profileAnswer);
-    Criteria criteria = new Criteria();
-    Answer criteriaAnswer = new Answer(question, Bool.TRUE);
+    Answer criteriaAnswer = new Answer(this.question, Bool.TRUE);
     Criterion criterion = new Criterion(criteriaAnswer, Weight.DontCare);
-    criteria.add(criterion);
+    this.criteria.add(criterion);
 
-    boolean matches = profile.matches(criteria);
+    boolean matches = profile.matches(this.criteria);
 
     assertTrue(matches);
   }
